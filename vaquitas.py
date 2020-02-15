6275520c70a603cbd6c24f245778dbc67b6a09d9
DarioSabatini <beunomax@yahoo.com.ar>
2020-02-15 11:46:35 -0300

    empezado

--- vaquitas.py
+++ vaquitas.py
@@ -5,13 +5,22 @@ import threading
 
 inicioPuente = 10
 largoPuente = 20
-
+semaforo = threading.Semaphore(14)
+semCruzar = threading.Semaphore(2)
+class Puente():
+  inicio = 0
+  largo = 0
+  def __init__(self,inicio,largo):
+    self.inicio = inicio
+    self.largo = largo
+  def dibujar(self):
+    print(' ' * self.inicio + '=' * self.largo)
 class Vaca(threading.Thread):
   def __init__(self):
     super().__init__()
     self.posicion = 0
     self.velocidad = random.uniform(0.1, 0.5)
-
+    # self.velocidad = 0.0001
   def avanzar(self):
     time.sleep(self.velocidad)
     self.posicion += 1
@@ -21,10 +30,21 @@ class Vaca(threading.Thread):
 
   def run(self):
     while(True):
-      self.avanzar()
-
+      if self.posicion < inicioPuente:
+        semaforo.acquire()
+        try:
+          self.avanzar()
+        finally:
+          semaforo.release()
+      else:
+        while self.posicion < inicioPuente+largoPuente:     
+          semCruzar.acquire()
+          try:
+            self.avanzar()
+          finally:
+            semCruzar.release()
 vacas = []
-for i in range(5):
+for i in range(7):
   v = Vaca()
   vacas.append(v)
   v.start()
@@ -33,14 +53,15 @@ def cls():
   os.system('cls' if os.name=='nt' else 'clear')
 
 def dibujarPuente():
-  print(' ' * inicioPuente + '=' * largoPuente)
+  print(' ' * inicioPuente + '=' * largoPuente + ' ' * inicioPuente + '=' * largoPuente)
 
+puente1 = Puente(11,24)
 while(True):
   cls()
   print('Apretá Ctrl + C varias veces para salir...')
   print()
-  dibujarPuente()
+  puente1.dibujar()
   for v in vacas:
     v.dibujar()
-  dibujarPuente()
+  puente1.dibujar()
   time.sleep(0.2)
